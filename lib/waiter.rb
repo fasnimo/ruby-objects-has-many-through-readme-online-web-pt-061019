e2df4245f1e45e5f7c28d68af04f14ef10884238
Rene Cocom <fasnimo@gmail.com>
2019-08-01 05:21:55 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,21 @@
 class Waiter
+  attr_accessor :name, :years
+
+  @@all = []
+
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
+  end
+
 
 end
